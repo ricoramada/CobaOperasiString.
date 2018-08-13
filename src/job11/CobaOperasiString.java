f01c12cebf9489056755e5a3a69cdbba2678b994
ASUS <ASUS@DESKTOP-MSAH19U>
2018-08-13 03:17:45 -0700

    Initial Commit

---
+++ src/job11/CobaOperasiString.java
@@ -0,0 +1,8 @@
+package job11;
+
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        String identitas = "Mukhamad Rico Ramada/ XRPL3/ 27";
+        System.out.println("identitas : " + identitas);
+    }
+}
