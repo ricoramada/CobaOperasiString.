efec782719fb22c3515e2d0801853d4b56f45814
ASUS <ASUS@DESKTOP-MSAH19U>
2018-08-13 03:56:50 -0700

    Coba method length & is Empty.

--- src/job11/CobaOperasiString.java
+++ src/job11/CobaOperasiString.java
@@ -4,5 +4,10 @@ public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Mukhamad Rico Ramada/ XRPL3/ 27";
         System.out.println("identitas : " + identitas);
+        String x = "Operasi";
+        System.out.println("Isi variabel x : " + x);
+        System.out.println("\"" + x + "\"panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
     }
 }
